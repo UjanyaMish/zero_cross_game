81c51559ac6cc3a9356b14316687cd3fcfe51ee6
UjnayaMish <deykafirefox217@gmail.com>
2023-12-25 20:42:00 +0500

    Немного правок и кода

--- Scripts/CardDeck.cs
+++ Scripts/CardDeck.cs
@@ -3,25 +3,48 @@ using System;
 
 public partial class CardDeck : Node2D
 {
+    public int x = 0;
+    public int y = 0;
+    private bool mouse_on_top = false;
+    private bool lazy = true;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-        /*if (Input.IsMouseButtonPressed(MouseButton.Left))
-		{
-            Tween tween = GetTree().CreateTween();
-            CardDeck card_copy = CardDeck.Duplicate();
-            GetParent().AddChild(card_copy);
-            tween.TweenProperty(this, "position", card_copy.Position + new Vector2(16, 0), 0.2f).SetEase(Tween.EaseType.Out);
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (Input.IsMouseButtonPressed(MouseButton.Left) && mouse_on_top)
+        {
+            if (lazy)
+            {
+                Tween tween = GetTree().CreateTween();
+                Neko newNeko = CreateCat(1, x, y);
+                tween.TweenProperty(newNeko.unit, "position", newNeko.unit.Position + new Vector2(272, 266), 0.2f).SetEase(Tween.EaseType.Out);
+                lazy = false;
+            }
+        }
+        else
+        {
+            lazy = true;
+        }
+    }
 
-			//нужно как-то достать из будущих юнитов инфу, какие они будут (как минимум изображение), чтобы мы смогли
-			//показывать игроку, какие юниты у него есть на картах. Плюс нужно добавить нормальную анимацию передвижения
-			//с коллоды на планочку снизу (найти их позицию)
-        }*/
+    public void _on_mouse_entered()
+    {
+        mouse_on_top = true;
+    }
+
+    public void _on_mouse_exited()
+    {
+        mouse_on_top = false;
+    }
+
+    public Neko CreateCat(int team, int x, int y)
+    {
+        Archer nw = new(team, x, y, this);
+        return nw;
     }
 }
